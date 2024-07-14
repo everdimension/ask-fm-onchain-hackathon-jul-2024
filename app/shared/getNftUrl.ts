27d44bddc8c64009280e7dc5e36d0d21adfed712
everdimension <everdimension@gmail.com>
2024-07-14 13:56:43 +0200

    update contract address

--- app/shared/getNftUrl.ts
+++ app/shared/getNftUrl.ts
@@ -1,5 +1,5 @@
-const collectionAddress = "0xa3915795cfb58e8e59bbc5b9723ee3661529dff0";
-
+// const collectionAddressBase = "0xa3915795cfb58e8e59bbc5b9723ee3661529dff0";
+const collectionAddressCelo = "0x099a9f22D1bcB5A90E0B8fa0Cb0fC47350F113ea";
 export function getNftUrl(tokenId: string) {
-  return `https://app.zerion.io/nfts/base/${collectionAddress}:${tokenId}`;
+  return `https://app.zerion.io/nfts/celo/${collectionAddressCelo}:${tokenId}`;
 }
